2d96f7e468f862deb5f1d8f45e16e5974f97ec43
sk <lylapp@163.com>
2018-12-08 00:56:07 +0800

    初始化

--- ProvisionDart/GeneratePreviewForURL.c
+++ ProvisionDart/GeneratePreviewForURL.c
@@ -1,7 +1,7 @@
 #include <CoreFoundation/CoreFoundation.h>
 #include <CoreServices/CoreServices.h>
 #include <QuickLook/QuickLook.h>
-
+#import <AppKit/AppKit.h>
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options);
 void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
 
@@ -14,6 +14,26 @@ void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
     // To complete your generator please implement the function GeneratePreviewForURL in GeneratePreviewForURL.c
+    NSURL *URL = (__bridge NSURL *)url;
+NSString * dartContent = [[NSString alloc] initWithContentsOfURL:URL
+                                                        encoding:NSUTF8StringEncoding
+                                                           error:nil];
+    
+    NSDictionary *properties = @{ // properties for the HTML data
+        (__bridge NSString *)kQLPreviewPropertyTextEncodingNameKey : @"UTF-8",
+        (__bridge NSString *)kQLPreviewPropertyMIMETypeKey : @"text/html" };
+//    DART_CONTENT
+    NSURL *bundleURL = [[NSBundle bundleWithIdentifier:@"com.sk.ProvisionDart"] URLForResource:@"heightlight" withExtension:@"bundle"];
+    NSURL *templateURL =  [[[NSBundle alloc] initWithURL:bundleURL] URLForResource:@"Template" withExtension:@"html"];
+    
+    NSMutableString * templateContent = [[NSMutableString alloc] initWithContentsOfURL:templateURL
+encoding:NSUTF8StringEncoding error:nil];
+    
+    NSString *finalContent = [templateContent stringByReplacingOccurrencesOfString:@"$DART_CONTENT" withString:dartContent];
+
+    
+    QLPreviewRequestSetDataRepresentation(preview, (__bridge CFDataRef)[finalContent dataUsingEncoding:NSUTF8StringEncoding], kUTTypeHTML, (__bridge CFDictionaryRef)properties);
+    
     return noErr;
 }
 
--- ProvisionDart/GenerateThumbnailForURL.c
+++ ProvisionDart/GenerateThumbnailForURL.c
@@ -1,7 +1,7 @@
 #include <CoreFoundation/CoreFoundation.h>
 #include <CoreServices/CoreServices.h>
 #include <QuickLook/QuickLook.h>
-
+#import <AppKit/AppKit.h>
 OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize);
 void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbnail);
 
@@ -14,6 +14,24 @@ void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbn
 OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize)
 {
     // To complete your generator please implement the function GenerateThumbnailForURL in GenerateThumbnailForURL.c
+    
+    NSURL *iconURL = [[NSBundle bundleWithIdentifier:@"com.sk.ProvisionDart"] URLForResource:@"ic_launcher" withExtension:@"png"];
+
+    NSImage *appIcon = [[NSImage alloc] initWithContentsOfURL:iconURL];
+    NSSize canvasSize = appIcon.size;
+    NSRect renderRect = NSMakeRect(0.0, 0.0, appIcon.size.width, appIcon.size.height);
+
+    CGContextRef _context = QLThumbnailRequestCreateContext(thumbnail, canvasSize, false, NULL);
+    if (_context) {
+        NSGraphicsContext *_graphicsContext = [NSGraphicsContext graphicsContextWithCGContext:(void *)_context flipped:NO];
+        
+        [NSGraphicsContext setCurrentContext:_graphicsContext];
+       
+            [appIcon drawInRect:renderRect];
+        
+        QLThumbnailRequestFlushContext(thumbnail, _context);
+        CFRelease(_context);
+    }
     return noErr;
 }
 
